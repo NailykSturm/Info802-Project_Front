c11095f6d8209fe4da4898add5fb2646a3d27244
Kylian BACHELET <kykydesavoie@hotmail.com>
2023-03-14 10:32:34 +0100

    Select city when enter name

--- journey-planner/src/components/JourneyForm.vue
+++ journey-planner/src/components/JourneyForm.vue
@@ -23,6 +23,7 @@ export default defineComponent({
 
         const countryCodeList = ref([
             {
+                'key': 'default',
                 'label': 'Sélectionnez un pays',
                 'value': null
             }
@@ -42,19 +43,59 @@ export default defineComponent({
                 console.log(err);
             });
 
+        const cityList = ref([
+            {
+                'key': 'default',
+                'label': 'Sélectionnez une ville',
+                'value': null,
+                'info': {
+                    'country': 'none',
+                    'coord': [],
+                }
+            }
+        ]);
+        const showCityListStart = ref(false);
+        const showCityListEnd = ref(false);
         function handleSelectCountryCode(value) {
             CCL.value.find((country) => {
                 country.cca2 === value ? journey.journey.value.find((step) => { step.countryCode === value ? step.coord = country.latlng : null; }) : null;
             });
+        }
+        function handleSelectCity(key, value) {
+            console.log('poulet');
+            console.log(key);
+            console.log(value);
+        }
+        function handleKeyUp(key, step) {
+            if (key.code === 'Enter') {
+                if (step === 'start') {
+                    showCityListStart.value = true;
+                } else if (step === 'end') {
+                    showCityListEnd.value = true;
+                }
+            }
+        }
+        function handleBlur(key, step) {
+            if (step === 'start') {
+                showCityListStart.value = false;
+            } else if (step === 'end') {
+                showCityListEnd.value = false;
+            }
         }
         return {
             journeyStore,
             journey,
             countryCodeList,
             CCL,
+            cityList,
+            showCityListStart,
+            showCityListEnd,
             addStep,
             search,
             handleSelectCountryCode,
+            handleSelectCity,
+            handleKeyUp,
+            handleBlur,
         };
     }
 });
@@ -67,12 +108,18 @@ export default defineComponent({
     end : {{ journey.end }}<br>
     <!-- <n-space> -->
     <!-- <n-space> -->
-    <n-input v-model:value="journey.start.value.location" type="text" placeholder="Ville de départ" />
+    <n-dropdown :show="showCityListStart" :options="cityList" @select="handleSelectCity">
+        <n-input v-model:value="journey.start.value.location" type="text" placeholder="Ville de départ"
+            @keyup="handleKeyUp($event, 'start')" @blur="handleBlur($event, 'start')" />
+    </n-dropdown>
     <n-select v-model:value="journey.start.value.countryCode" filterable :options="countryCodeList"
         @update:value="handleSelectCountryCode" />
     <!-- </n-space> -->
     <!-- <n-space> -->
-    <n-input v-model:value="journey.end.value.location" type="text" placeholder="Ville d'arrivée" />
+    <n-dropdown :show="showCityListEnd" :options="cityList" @select="handleSelectCity">
+        <n-input v-model:value="journey.end.value.location" type="text" placeholder="Ville d'arrivée"
+            @keyup="handleKeyUp($event, 'end')" @blur="handleBlur($event, 'end')" />
+    </n-dropdown>
     <n-select v-model:value="journey.end.value.countryCode" filterable :options="countryCodeList"
         @update:value="handleSelectCountryCode" />
     <!-- </n-space> -->
