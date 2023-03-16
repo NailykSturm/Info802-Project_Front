11b8ccc78f6f81757bf8cf9e984f06b24ef00a7f
Kylian BACHELET <kykydesavoie@hotmail.com>
2023-03-16 08:52:52 +0100

    Add docker + env.sample

---
+++ journey-planner/src/env.sample.js
@@ -0,0 +1,12 @@
+export const MAP_API_KEY = "";
+
+const CAR_CLIENT_ID = "";
+const CAR_API_ID = "";
+export const CHARGETRIP_API_HEADERS = {
+    "x-client-id": CAR_CLIENT_ID,
+    "x-app-id": CAR_API_ID,
+    "Content-Type": "application/json",
+};
+
+const SOAP_API_PORT = 0;
+export const SOAP_API_URL = `${SOAP_API_PORT}`;
